00a7e2d2cea60f5baed5fc33f8194cb219f6e51f
Hendi Santika <hendisantika@gmail.com>
2018-08-05 05:52:01 +0700

    #3 - Add Role Entity

---
+++ src/main/java/com/hendisantika/springbootsecuritymysql/entity/Role.java
@@ -0,0 +1,54 @@
+package com.hendisantika.springbootsecuritymysql.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-security-mysql
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 05/08/18
+ * Time: 05.51
+ * To change this template use File | Settings | File Templates.
+ */
+@Entity
+@Table(name = "role")
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "role_id")
+    private Integer roleId;
+
+    @Column(name = "role_name")
+    private String role;
+
+    public Role() {
+    }
+
+    public Role(Integer roleId, String role) {
+        this.roleId = roleId;
+        this.role = role;
+    }
+
+    public Integer getRoleId() {
+        return roleId;
+    }
+
+    public void setRoleId(Integer roleId) {
+        this.roleId = roleId;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+}
