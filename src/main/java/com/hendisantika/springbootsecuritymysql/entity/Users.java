7f9232224deab4a2c205f302b0ca16022b250e7f
Hendi Santika <hendisantika@gmail.com>
2018-08-05 05:53:18 +0700

    #4 - Add Users Entity

---
+++ src/main/java/com/hendisantika/springbootsecuritymysql/entity/Users.java
@@ -0,0 +1,104 @@
+package com.hendisantika.springbootsecuritymysql.entity;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import java.util.Set;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-security-mysql
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 05/08/18
+ * Time: 05.52
+ * To change this template use File | Settings | File Templates.
+ */
+@Entity
+@Table(name = "user")
+public class Users {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "user_id")
+    private Integer userId;
+
+    @Column(name = "user_name")
+    private String username;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "active")
+    private int isActive;
+
+    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    @JoinTable(name = "user_roles", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private Set<Role> roles;
+
+    public Users() {
+    }
+
+    public Users(String username, String password, int isActive, Set<Role> roles) {
+        this.username = username;
+        this.password = password;
+        this.isActive = isActive;
+        this.roles = roles;
+    }
+
+    public Users(Users users) {
+        this.username = users.getUsername();
+        this.password = users.getPassword();
+        this.isActive = users.getIsActive();
+        this.roles = users.getRoles();
+    }
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getIsActive() {
+        return isActive;
+    }
+
+    public void setIsActive(int isActive) {
+        this.isActive = isActive;
+    }
+
+    public Set<Role> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Set<Role> roles) {
+        this.roles = roles;
+    }
+}
