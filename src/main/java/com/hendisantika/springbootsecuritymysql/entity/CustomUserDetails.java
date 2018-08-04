608c8ee80b6945e7f1c28f063b4f05ba2a4e187b
Hendi Santika <hendisantika@gmail.com>
2018-08-05 05:54:32 +0700

    #5 - Add Custome Users Details

---
+++ src/main/java/com/hendisantika/springbootsecuritymysql/entity/CustomUserDetails.java
@@ -0,0 +1,64 @@
+package com.hendisantika.springbootsecuritymysql.entity;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.stream.Collectors;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-security-mysql
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 05/08/18
+ * Time: 05.53
+ * To change this template use File | Settings | File Templates.
+ */
+public class CustomUserDetails extends Users implements UserDetails {
+
+    public CustomUserDetails(final Users users) {
+        super(users);
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+
+        return getRoles()
+                .stream()
+                .map(role -> new SimpleGrantedAuthority("ROLE_" + role.getRole()))
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public String getPassword() {
+        return super.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return super.getUsername();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
